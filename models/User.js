a4c7e1bb73443ea510e94f8e8a46c841366b71ef
inqilobchi <hieojody@gmail.com>
2025-12-20 10:05:31 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   userId: { type: Number, required: true, unique: true },
+
   plan: { type: String, default: 'free' },
   stars: { type: Number, default: 0 },
+
   referralCount: { type: Number, default: 0 },
   referralCode: { type: String, unique: true },
   referrerId: { type: Number, default: null },
+
   theme: { type: String, default: 'midnight' },
+
+  isAdmin: { type: Boolean, default: false }, 
+  banned: { type: Boolean, default: false },  
+
   createdAt: { type: Date, default: Date.now }
 });
 
