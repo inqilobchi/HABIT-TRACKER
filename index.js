3865376e861c5d370e988c7cffc559e69245d64d
inqilobchi <hieojody@gmail.com>
2026-03-02 08:22:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -389,7 +389,7 @@ bot.on('callback_query', async (query) => {
   const user = await getUser(userId);
 
   if (data === 'referral') {
-    const link = `https://t.me/uzkaizen_bot?start=${user.referralCode}`;
+    const link = `https://t.me/Kunlik_RejalarimBot?start=${user.referralCode}`;
     let message = `🎁 Do\'stlarni taklif qiling!\n\nSizning kodingiz: ${user.referralCode}\nLink: ${link}\n\nHar do\'st uchun 1000 ⭐\nReferral soni: ${user.referralCount}`;
 
     const keyboard = { inline_keyboard: [] };
